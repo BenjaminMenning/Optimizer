38ccececda5ba7bb7c70ef13b5cad5513ca2d072
JDiercks14 <JDiercks14@jdiercks146.workstations.winona.edu>
2016-03-22 13:36:25 -0500

    Working on Height sorter

--- src/Optimizer.java
+++ src/Optimizer.java
@@ -9,8 +9,20 @@
  *
  * @author Seema
  */
+
+
+import java.util.ArrayList;
+
+
 public class Optimizer {
 
+    
+    
+    
+    public static ArrayList<Product> productList = new ArrayList<>();
+    
+    //Height sorting clone of "productList"
+    public static ArrayList<Product> cloneList = new ArrayList(productList);
     /**
      * @param args the command line arguments
      */
@@ -20,4 +32,40 @@ public class Optimizer {
         // TODO code application logic here
     }
     
-}
+    public static void heightSorter() {
+        
+        //ArrayList<Product> cloneList = new ArrayList(productList);
+        
+        
+        int smallInt = 0;
+        int j = 0;
+        int smallIntIndex = 0;
+        
+        for(int i=1; i < cloneList.size()-1; i++) {            
+            smallInt = (int) cloneList.get(i-1).getHeight();
+            smallIntIndex = (i-1);
+            
+            for(j=i; j < cloneList.size(); j++){
+                if(cloneList.get(j).getHeight() < smallInt){
+                    smallInt = (int) cloneList.get(j).getHeight();
+                    smallIntIndex = j;
+                }//end if                
+            }//end for
+                   
+            swap(i-1, smallIntIndex);
+            
+        }//end for
+        
+        
+    }//end heightSorter
+
+    private  void swap(int sourceIndex, int destIndex) {
+        int temp = cloneList.get(destIndex);
+        cloneList.set(destIndex, cloneList.get(sourceIndex));
+        cloneList.set(sourceIndex, temp); 
+    }
+    
+    
+    
+    
+}//end 
